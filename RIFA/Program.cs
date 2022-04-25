d56d68766320a4cd72940c6e7328f948a019a160
DELL <DELL@DELL>
2022-04-25 17:36:29 -0600

    Agregar archivos de proyecto.

---
+++ RIFA/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+
+using RIFA.clases;
+
+LaRifa rifa = new LaRifa();
+
+string[] nom = { "Rldin", "Samuel", "Eduardo", "David", "Jaime","Melissa","Stephanie","Mario","Liva","Kevin"};
+
+
+rifa.sorteo(nom, 8);
---
+++ RIFA/clases/LaRifa.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RIFA.clases
+{
+    internal class LaRifa
+    {
+
+
+        public int alealtorio(int no1, int no2)
+        {
+            Random random = new Random();
+            return random.Next(1,9);
+
+          
+        }
+
+
+        public void sorteo(string[] participantes, int  premio )
+        {
+
+            for (int i = 1; i <= premio; i++)
+            {
+
+
+
+                int final = alealtorio(0, participantes.Length);
+
+                
+
+               
+                Console.WriteLine($"Sorteo {i} Ganador = {final}");
+                Console.WriteLine($"El ganador es {participantes[final]}\n");
+
+            }
+
+
+
+        }
+
+    }
+
+}
